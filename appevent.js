be91a7ee165e94f18fa7a0128596574f5b96451d
pramodC135 <pramodmadhushan4@gmail.com>
2021-05-09 03:50:42 +0530

    event arg

--- appevent.js
+++ appevent.js
@@ -2,12 +2,14 @@ const EventEmitter = require('events');
 const emitter = new EventEmitter();
 
 //Register a listener
-emitter.on('messageLogged', function(){
-    console.log('Listener called');
+emitter.on('messageLogged', (arg) => {
+    console.log('Listener called', arg);
 });
 
 //Raise an event
-emitter.emit('messageLogged');
+emitter.emit('messageLogged', {id: 1, url: 'http://' });
+
+//Raise: logged (data: message)
 
 //class MyEmitter extends EventEmitter {}
 
